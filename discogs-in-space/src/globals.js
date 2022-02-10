5b6c6e59749f9e173578685e890c84e2877cea5e
Hayden Anderson <anderson.hayden@gmail.com>
2022-02-10 16:57:02 -0500

    test and basic structure

---
+++ discogs-in-space/src/globals.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://api.discogs.com'
+
+export const key = 'JULdBFWPKHretQoEqPnN'
+export const secret = 'IbeXFJAJOnUXXbYEVBgGREjBSQFbtZOz'
+
+// Consumer Key	JULdBFWPKHretQoEqPnN
+// Consumer Secret	IbeXFJAJOnUXXbYEVBgGREjBSQFbtZOz
+// Request Token URL	https://api.discogs.com/oauth/request_token
+// Authorize URL	https://www.discogs.com/oauth/authorize
+// Access Token URL	https://api.discogs.com/oauth/access_token
